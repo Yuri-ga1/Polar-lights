91f304f05b2ead16cafd754c61211d49a285f737
Yuri-ga1 <regota09@gmail.com>
2026-05-04 13:49:58 +0800

    optimize simurg processor time-based loading

--- app/simurg/simurg_processor.py
+++ app/simurg/simurg_processor.py
@@ -53,6 +53,47 @@ class SimurgProcessor(BaseProcessor):
         except ValueError as error:
             supported = ", ".join(p.value for p in DataProduct)
             raise ValueError(f"Неизвестный тип продукта: {product_type}. Поддерживаются: {supported}") from error
+        
+    @classmethod
+    def _format_time_key(cls, value: str | datetime) -> str:
+        if isinstance(value, str):
+            value = datetime.strptime(value, "%Y-%m-%d %H:%M:%S")
+
+        value = cls._normalize_time(value)
+
+        return value.strftime(TIME_FORMAT)
+    
+    @classmethod
+    def _resolve_time_keys(
+        cls,
+        data_group,
+        times: list[str | datetime],
+    ) -> list[str]:
+        available_keys = set(data_group.keys())
+        selected_keys: list[str] = []
+
+        for value in times:
+            requested_key = cls._format_time_key(value)
+
+            if requested_key in available_keys:
+                selected_keys.append(requested_key)
+                continue
+
+            requested_dt = cls._normalize_time(
+                datetime.strptime(
+                    requested_key,
+                    TIME_FORMAT,
+                )
+            )
+
+            nearest_key = min(
+                available_keys,
+                key=lambda key: abs(cls._parse_time(key) - requested_dt),
+            )
+
+            selected_keys.append(nearest_key)
+
+        return selected_keys
 
     def _find_file(
         self,
@@ -71,8 +112,8 @@ class SimurgProcessor(BaseProcessor):
         self,
         date_value: Union[str, date, datetime],
         product_type: str | DataProduct = DataProduct.ROTI,
+        times: Optional[list[str | datetime]] = None,
     ) -> Optional[Dict[datetime, NDArray]]:
-        
         target_date = self._coerce_date(date_value)
         normalized_product = self._normalize_product(product_type)
         file_path = self._find_file(target_date, normalized_product)
@@ -86,9 +127,21 @@ class SimurgProcessor(BaseProcessor):
             with h5py.File(file_path, "r") as handle:
                 if "data" not in handle:
                     return None
-                for str_time in list(handle["data"]):
+
+                data_group = handle["data"]
+
+                if times is None:
+                    selected_keys = list(data_group.keys())
+                else:
+                    selected_keys = self._resolve_time_keys(
+                        data_group=data_group,
+                        times=times,
+                    )
+
+                for str_time in selected_keys:
                     parsed_time = self._parse_time(str_time)
-                    data[parsed_time] = handle["data"][str_time][:]
+                    data[parsed_time] = data_group[str_time][:]
+
         except Exception:
             return None
 
